7342d615de9a58e4e8deb8ad1309002d679c97a1
ihor_debelyi <s15293@pjwstk.edu.pl>
2018-11-29 12:54:33 +0100

    added assignment 7

--- TIN/Assignment 7/7a.js
+++ TIN/Assignment 7/7a.js
@@ -1,20 +1,33 @@
 "use strict";
 
 const operations = {
-  "/multiply": (a, b) => {return a * b;},
-  "/divide": (a, b) => {return a / b;},
-  "/add": (a, b) => {return a + b;},
-  "/substract": (a, b) => {return a - b;}
+  "/multiply": {
+    sign: "*",
+    action: (a, b) => {
+      return a * b;
+    }
+  },
+  "/divide": {
+    sign: "/",
+    action: (a, b) => {
+      return a / b;
+    }
+  },
+  "/add": {
+    sign: "+",
+    action: (a, b) => {
+      return a + b;
+    }
+  },
+  "/substract": {
+    sign: "-",
+    action: (a, b) => {
+      return a - b;
+    }
+  }
 };
 
-const dict = {
-  "/multiply": '*',
-  "/divide": '/',
-  "/add": '+',
-  "/substract": '-'
-};
-
-let re = new RegExp('^[0-9]+$');
+let re = new RegExp("^[0-9]+$");
 
 let http = require("http");
 let url = require("url");
@@ -29,11 +42,11 @@ let server = http.createServer(function(req, res) {
     let param2 = req.headers["parameter2"];
     let r1 = re.test(param1);
     let r2 = re.test(param2);
-    if(!r1 || !r2){
+    if (!r1 || !r2) {
       res.statusCode = 400;
-      res.end('Invalid parameters');
+      res.end("Invalid parameters");
     }
-    let result = operations[operation](parseInt(param1), parseInt(param2));
+    let result = operations[operation].action(parseInt(param1), parseInt(param2));
     res.end(formatRepsonse(operation, param1, param2, result));
   } catch (er) {
     res.end("Server error");
@@ -42,7 +55,7 @@ let server = http.createServer(function(req, res) {
 
 server.listen(8000, "localhost");
 
-function formatRepsonse(operation, param1, param2, result){
+function formatRepsonse(operation, param1, param2, result) {
   return `<!DOCTYPE html><html lang="en"> <head> <meta charset="UTF-8" /> <title>Result</title> <style> body { background: #242831; color: #fafbff; font-family: "Source Sans Pro"; } .container { height: 100%; width: 100%; display: flex; position: fixed; align-items: center; justify-content: center; } .container > div { font-size: 100px; } </style> </head> <body> <div class="container">
-  <div>${param1} ${dict[operation]} ${param2} = ${result}</div></div> </body></html>`;
+  <div>${param1} ${operations[operation].sign} ${param2} = ${result}</div></div> </body></html>`;
 }
